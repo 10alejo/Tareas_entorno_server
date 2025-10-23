b8e89eea026326b3854155c126ef4cbb6a9b71d5
Alejandro <alejandromartindomingo06@gmail.com>
2025-10-23 13:01:01 +0200

    commit final del proyecto ejForo

--- ejForo/app/funciones.php
+++ ejForo/app/funciones.php
@@ -1,6 +1,40 @@
 <?php
-function usuarioOk($usuario, $contraseña) :bool {
-  
-   return ($usuario == "pepe");
-    
+function usuarioOk($usuario, $contraseña): bool {
+    return (strlen($usuario) >= 8 && $usuario == strrev($contraseña));
 }
+
+function contarPalabras($cadena) {
+    return str_word_count($cadena, 0);
+}
+
+function letraMasrepetida($cadena) {
+    $vecesMax = 0;
+    $letraMax = 'a';
+    $tamaño = strlen($cadena);
+    for ($i = 0; $i < $tamaño; $i++) {
+        $veces = 1;
+        $letrai = $cadena[$i];
+        for ($j = $i + 1; $j < $tamaño; $j++) {
+            if ($letrai == $cadena[$j]) {
+                $veces++;
+            }
+        }
+        if ($veces > $vecesMax) {
+            $letraMax = $letrai;
+            $vecesMax = $veces;
+        }
+    }
+    return $letraMax;
+}
+
+function palabraMasrepetida($cadena) {
+    $palabras = str_word_count($cadena, 1);
+    $palabrasveces = array_count_values($palabras);
+    asort($palabrasveces);
+    return array_key_last($palabrasveces);
+}
+
+function limpiarEntrada(&$cadena) {
+    $cadena = htmlspecialchars($cadena);
+}
+?>
